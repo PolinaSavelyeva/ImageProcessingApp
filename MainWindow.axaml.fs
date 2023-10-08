8c77735aa54f05f75c8a7b337ac45e92269c86bc
PolinaSavelyeva <lissava00@gmail.com>
2023-10-08 20:26:54 +0300

    Add button click action

--- MainWindow.axaml.fs
+++ MainWindow.axaml.fs
@@ -3,12 +3,17 @@ namespace ImageProcessingApp
 open Avalonia
 open Avalonia.Controls
 open Avalonia.Markup.Xaml
+open Avalonia.Interactivity
+open System.Diagnostics
 
 type MainWindow () as this = 
     inherit Window ()
 
     do this.InitializeComponent()
     
+    member this.ButtonClicked(source: obj, args: RoutedEventArgs ) =
+        Debug.WriteLine("Click!")
+        
     member private this.InitializeComponent() =
 #if DEBUG
         this.AttachDevTools()
@@ -17,3 +22,7 @@ type MainWindow () as this =
         this.Height <- 896
         
         AvaloniaXamlLoader.Load(this)
+
+        
+
+        
